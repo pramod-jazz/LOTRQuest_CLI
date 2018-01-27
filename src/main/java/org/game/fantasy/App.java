22fd6c5f7790edc5bcc3958417dab796218494dc
Pramod Nikam <pramod.nikam1@gmail.com>
2018-01-27 19:48:44 +0530

    First Project Structure Commit

---
+++ src/main/java/org/game/fantasy/App.java
@@ -0,0 +1,13 @@
+package org.game.fantasy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
