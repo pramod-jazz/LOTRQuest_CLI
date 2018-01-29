7d5132639f8ef3468bc52fe419cdd90c8fbf151c
Pramod Nikam <pramod.nikam1@gmail.com>
2018-01-30 03:20:20 +0530

    Exception handling setup for game.

--- src/main/java/org/game/fantasy/exceptions/GameException.java
+++ src/main/java/org/game/fantasy/exceptions/GameException.java
@@ -1,6 +1,22 @@
 package org.game.fantasy.exceptions;
 
+import org.apache.log4j.Logger;
+
 public class GameException extends RuntimeException {
-       
+ 
+	
+	final static Logger logger = Logger.getLogger(GameException.class);
+	
+	 public GameException(String message) {
+         super(message);
+         
+         
+     }
+     public GameException(String message, Throwable throwable) {
+         super(message, throwable);
+         logger.error(throwable);
+         
+     }
+	
 	 
 }
